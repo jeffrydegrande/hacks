543ce3ca1096d39a87a6366ed950b95828d1c1e1
Jeffry Degrande <jeffry@jeffrydegrande.com>
2013-02-05 19:53:29 -0200

    organize files

---
+++ lib/standard_deviation.rb
@@ -0,0 +1,19 @@
+class StandardDeviation
+  # Calculate the standard deviation of a series of integers.
+  #
+  def calculate series
+    mean        = mean(series)
+    differences = series.map { |value| difference_to_mean(value, mean) }
+    Math.sqrt(mean(differences))
+  end
+
+  # Calculate the mean of a series of integers
+  #
+  def mean(series)
+    series.inject(&:+) / series.length
+  end
+
+  def difference_to_mean(value, mean)
+    ( value - mean ) ** 2
+  end
+end
--- spec/standard_deviation_spec.rb
+++ spec/standard_deviation_spec.rb
@@ -1,22 +1,4 @@
-class StandardDeviation
-  # Calculate the standard deviation of a series of integers.
-  #
-  def calculate series
-    mean        = mean(series)
-    differences = series.map { |value| difference_to_mean(value, mean) }
-    Math.sqrt(mean(differences))
-  end
-
-  # Calculate the mean of a series of integers
-  #
-  def mean(series)
-    series.inject(&:+) / series.length
-  end
-
-  def difference_to_mean(value, mean)
-    ( value - mean ) ** 2
-  end
-end
+require './lib/standard_deviation'
 
 describe "standard deviation" do
   let (:series) { [2,4,4,4,5,5,7,9] }
