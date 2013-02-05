fbadfbab6ed6a2762a869ba9c794341718000869
Jeffry Degrande <jeffry@jeffrydegrande.com>
2013-02-05 20:49:17 -0200

    apply extract method

--- lib/rounded_currency.rb
+++ lib/rounded_currency.rb
@@ -1,20 +1,22 @@
 def rounded_currency value
-  val = value.to_f.abs
-  rounded = if val > 1_000_000_000
-              "%0.1fB" % [val / 1_000_000_000]
-            elsif val > 1_000_000
-              "%0.1fM" % [val / 1_000_000]
-            elsif val > 10_000
-              "%0.0fK" % [val / 1_000]
-            elsif val > 1_000
-              "%0.1fK" % [val / 1_000]
-            else
-              "%0.0f" % val
-            end
-
+  rounded = round_with_string(value.to_f.abs)
   if value < 0
     "($#{rounded})"
   else
     "$#{rounded}"
+  end
+end
+
+def round_with_string value
+  if value > 1_000_000_000
+    "%0.1fB" % [value / 1_000_000_000]
+  elsif value > 1_000_000
+    "%0.1fM" % [value / 1_000_000]
+  elsif value > 10_000
+    "%0.0fK" % [value / 1_000]
+  elsif value > 1_000
+    "%0.1fK" % [value / 1_000]
+  else
+    "%0.0f" % value
   end
 end
